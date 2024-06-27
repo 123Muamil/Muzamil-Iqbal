c473eb4eafe75b83b7eb956139f904ad165b6136
Muzamil Iqbal <107328196+123Muamil@users.noreply.github.com>
2024-06-27 11:14:54 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -34,7 +34,7 @@ const App = () => {
   smooth 
   component={<i className="lni lni-chevron-up"></i>} 
   className='scroll-top' 
-
+   style={{background-color: "#2ed06e"}}
 />
 {
         True?   <Preloader/>:
